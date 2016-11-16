37ae63de0528fade334fd1adbab3815951348f2c
walkerdrewett <walker.drewett@gmail.com>
2016-11-15 18:02:05 -0800

    11/15/16 - AlbumDetails and AlbumIndex

--- Team9/Models/Album.cs
+++ Team9/Models/Album.cs
@@ -16,11 +16,11 @@ namespace Team9.Models
         [Required(ErrorMessage = "Please enter a valid name")]
         public String AlbumName { get; set; }
 
-        [Required(ErrorMessage = "Please enter a valid genre")]
-        public virtual List<Genre> AlbumGenre { get; set; }
 
         [Required(ErrorMessage = "Please enter a valid price")]
         public Decimal AlbumPrice { get; set; }
+
+        public virtual List<Genre> AlbumGenre { get; set; }
 
         public virtual List<Artist> AlbumArtist { get; set; }
 
